e9a4be0a37183d0fc07913d05b34c06644d4a908
John Byrd <jbyrd@giganticsoftware.com>
2019-07-19 12:57:25 -0700

    Rethink instruction formats.  Able to parse two-byte LDA assembly instruction. First attempt at LDA instruction... operand handling incorrect though Added isImmediate() to MOSAsmParser Added m_has65SC02Insns to MOSSubtarget More predicate options added. More work on parsing an operand. Initial sketches of some fundamental instruction setups

--- llvm-cache.cmake
+++ llvm-cache.cmake
@@ -1,3 +1,3 @@
 cmake_minimum_required (VERSION 2.6)
-set(LLVM_ENABLE_PROJECTS "clang;clang-tools-extra;libcxx;libcxxabi;lldb;compiler-rt;lld" CACHE STRING "")
+set(LLVM_ENABLE_PROJECTS "clang;clang-tools-extra;libcxx;libcxxabi;compiler-rt;lld" CACHE STRING "")
 set(LLVM_EXPERIMENTAL_TARGETS_TO_BUILD "AVR;MOS" CACHE STRING "")
