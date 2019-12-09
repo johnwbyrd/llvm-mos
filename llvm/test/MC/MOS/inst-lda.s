d63f068e2a9cebff0664343a97596d666959297a
John Byrd <jbyrd@giganticsoftware.com>
2019-12-08 18:14:49 -0800

    Lots of mistakes on decoding LDA instruction, but at least it's a place to start.

--- llvm/test/MC/MOS/inst-lda.s
+++ llvm/test/MC/MOS/inst-lda.s
@@ -1,4 +1,7 @@
 ; RUN: llvm-mc -triple mos -mcpu=mos-6502 -show-encoding < %s | FileCheck %s
 foo:
-  LDA   #10
+  lda #255
+  lda 255
+  lda 256
+  lda 65535
 ;; CHECK: lda   5         ; encoding: [0x01 0x05]
