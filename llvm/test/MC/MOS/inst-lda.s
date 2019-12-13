7c5f9c2c145e88ba201d1ae4d39f549a1e23cf44
John Byrd <jbyrd@giganticsoftware.com>
2019-12-12 17:47:49 -0800

    Get build working Add in working examples of LDA

--- llvm/test/MC/MOS/inst-lda.s
+++ llvm/test/MC/MOS/inst-lda.s
@@ -1,13 +1,14 @@
 ; RUN: llvm-mc -triple mos -mcpu=mos-6502 -show-encoding < %s | FileCheck %s
+
 foo:
   lda #255
-  lda 255
-  lda 256
+  lda 0xff
   lda 257
-  lda 258,x
-  lda 259,y
-  lda foo
   lda 65535
   lda #0
-  beq foo
-;; CHECK: lda   5         ; encoding: [0x01 0x05]
+
+;; CHECK: lda #255                        ; encoding: [0xa9,0xff]
+;; CHECK: lda 255                         ; encoding: [0xa5,0xff]
+;; CHECK: lda 257                         ; encoding: [0xad,0x01,0x01]
+;; CHECK: lda 65535                       ; encoding: [0xad,0xff,0xff]
+;; CHECK: lda #0                          ; encoding: [0xa9,0x00]
