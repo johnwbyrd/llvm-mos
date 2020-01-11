0f3cb06af3bf9c7e710e830ebe6fc45aea286f00
John Byrd <jbyrd@giganticsoftware.com>
2020-01-11 15:02:36 -0800

    check-llvm-mc-mos wants a check case in all tests

--- llvm/test/MC/MOS/ophis-hello-world.s
+++ llvm/test/MC/MOS/ophis-hello-world.s
@@ -1,7 +1,7 @@
 ; RUN: llvm-mc -triple mos -mcpu=mos-generic -show-encoding < %s | FileCheck %s
 ; source: https://michaelcmartin.github.io/Ophis/book/x162.html
 
-        ldx #0
+ldx	#$0                     ; CHECK: encoding: [0xa2,0x00]
 /*
 loop:   lda hello, x
         beq done
