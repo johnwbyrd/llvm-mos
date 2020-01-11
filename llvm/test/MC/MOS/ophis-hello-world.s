67f53c55539d4fa56bdf451865d277dafc8a620b
John Byrd <jbyrd@giganticsoftware.com>
2020-01-10 22:00:44 -0800

    Add Hello World from Ophis assembler

---
+++ llvm/test/MC/MOS/ophis-hello-world.s
@@ -0,0 +1,12 @@
+; RUN: llvm-mc -triple mos -mcpu=mos-generic -show-encoding < %s | FileCheck %s
+; source: https://michaelcmartin.github.io/Ophis/book/x162.html
+
+        ldx #0
+loop:   lda hello, x
+        beq done
+        jsr $ffd2
+        inx
+        bne loop
+done:   rts
+
+hello:  .byte "HELLO, WORLD!", 0
