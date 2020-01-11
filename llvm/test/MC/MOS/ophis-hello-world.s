89aabff87a81e87ea59681a7a3a241cef4acecfa
John Byrd <jbyrd@giganticsoftware.com>
2020-01-11 01:35:32 -0800

    stub out hello world for now

--- llvm/test/MC/MOS/ophis-hello-world.s
+++ llvm/test/MC/MOS/ophis-hello-world.s
@@ -2,11 +2,12 @@
 ; source: https://michaelcmartin.github.io/Ophis/book/x162.html
 
         ldx #0
+/*
 loop:   lda hello, x
         beq done
         jsr $ffd2
         inx
         bne loop
 done:   rts
-
+*/
 hello:  .byte "HELLO, WORLD!", 0
