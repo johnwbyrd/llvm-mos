251b66334a2da774f035e64bb8fbd3677e1d896f
John Byrd <john.byrd@sonance.com>
2025-05-27 14:20:49 -0700

    First attempt at overriding GDB remote registers. I hate all the hard-coding of the DWARF details, but it's all temporary. I had to introduce a new override into ThreadGDBRemote in order to get it all to work (GetRegisterInfoSP), but that's really the fault of the lldb developers for not permitting me to do everything with factories.

--- lldb/include/lldb/Target/ABI.h
+++ lldb/include/lldb/Target/ABI.h
@@ -152,6 +152,14 @@ public:
 
   static lldb::ABISP FindPlugin(lldb::ProcessSP process_sp, const ArchSpec &arch);
 
+  virtual lldb::RegisterContextSP
+  CreateRegisterContextForThread(
+      lldb_private::Thread &thread,
+      uint32_t concrete_frame_idx) const {
+    // Default: return nullptr, meaning use the default RegisterContext
+    return nullptr;
+  }
+
 protected:
   ABI(lldb::ProcessSP process_sp, std::unique_ptr<llvm::MCRegisterInfo> info_up)
       : m_process_wp(process_sp), m_mc_register_info_up(std::move(info_up)) {
