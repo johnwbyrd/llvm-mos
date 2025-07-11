768390bcabd7562289daabdbbd34ad6126565317
John Byrd <john.byrd@sonance.com>
2025-07-11 15:09:06 -0700

    Reformat to LLVM standards Add build support for lldb and llvm-debuginfo-analyzer
    Reformat files to LLVM standards

--- lldb/source/Plugins/ABI/MOS/LLDBMOSLog.h
+++ lldb/source/Plugins/ABI/MOS/LLDBMOSLog.h
@@ -5,20 +5,21 @@
 
 // Define log categories as an enum for type safety
 enum LLDBMOSLogCategory : uint64_t {
-    LLDB_MOS_LOG_NONE = 0,
-    LLDB_MOS_LOG_REG  = 1ull << 0,
-    LLDB_MOS_LOG_SYM  = 1ull << 1,
-    LLDB_MOS_LOG_ABI  = 1ull << 2,
-    LLDB_MOS_LOG_ALL  = 0xFFFFFFFFFFFFFFFFull
+  LLDB_MOS_LOG_NONE = 0,
+  LLDB_MOS_LOG_REG = 1ull << 0,
+  LLDB_MOS_LOG_SYM = 1ull << 1,
+  LLDB_MOS_LOG_ABI = 1ull << 2,
+  LLDB_MOS_LOG_ALL = 0xFFFFFFFFFFFFFFFFull
 };
 
 // Central macro: only logs if enabled for the category
-#define LLDB_MOS_LOG(CAT, ...) \
-    do { \
-        if (lldb_private::Log *log = lldb_private::GetLLDBMOSLogChannel().GetLog(CAT)) { \
-            LLDB_LOG(log, __VA_ARGS__); \
-        } \
-    } while (0)
+#define LLDB_MOS_LOG(CAT, ...)                                                 \
+  do {                                                                         \
+    if (lldb_private::Log *log =                                               \
+            lldb_private::GetLLDBMOSLogChannel().GetLog(CAT)) {                \
+      LLDB_LOG(log, __VA_ARGS__);                                              \
+    }                                                                          \
+  } while (0)
 
 // Helper macros for common categories
 #define LLDB_MOS_LOG_REG(...) LLDB_MOS_LOG(LLDB_MOS_LOG_REG, __VA_ARGS__)
@@ -27,6 +28,6 @@ enum LLDBMOSLogCategory : uint64_t {
 
 // Channel getter (implement in .cpp)
 namespace lldb_private {
-    lldb_private::Log::Channel &GetLLDBMOSLogChannel();
-    void RegisterLLDBMOSLogChannel();
-} 
+lldb_private::Log::Channel &GetLLDBMOSLogChannel();
+void RegisterLLDBMOSLogChannel();
+} // namespace lldb_private
