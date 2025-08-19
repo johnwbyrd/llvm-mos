9f9dc0045c91f1982ba95f40ebb55b0750bba637
David Sherwood <david.sherwood@arm.com>
2025-08-19 09:31:37 +0100

    Calculate cost of extracting last index in a scalable vector (#144086)
    There are a couple of places in the loop vectoriser where we
    want to calculate the cost of extracting the last lane in a
    vector. However, we wrongly assume that asking for the cost
    of extracting lane (VF.getKnownMinValue() - 1) is an accurate
    representation of the cost of extracting the last lane. For
    SVE at least, this is non-trivial as it requires the use of
    whilelo and lastb instructions.
    
    To solve this problem I have added a new
    getReverseVectorInstrCost interface where the index is used
    in reverse from the end of the vector. Suppose a vector has
    a given ElementCount EC, the extracted/inserted lane would be
    EC - 1 - Index. For scalable vectors this index is unknown at
    compile time. I've added a AArch64 hook that better represents
    the cost, and also a RISCV hook that maintains compatibility
    with the behaviour prior to this PR.
    
    I've also taken the liberty of adding support in vplan for
    calculating the cost of VPInstruction::ExtractLastElement.

--- llvm/lib/Analysis/TargetTransformInfo.cpp
+++ llvm/lib/Analysis/TargetTransformInfo.cpp
@@ -1152,6 +1152,15 @@ TargetTransformInfo::getVectorInstrCost(const Instruction &I, Type *Val,
   return Cost;
 }
 
+InstructionCost TargetTransformInfo::getIndexedVectorInstrCostFromEnd(
+    unsigned Opcode, Type *Val, TTI::TargetCostKind CostKind,
+    unsigned Index) const {
+  InstructionCost Cost =
+      TTIImpl->getIndexedVectorInstrCostFromEnd(Opcode, Val, CostKind, Index);
+  assert(Cost >= 0 && "TTI should not produce negative costs!");
+  return Cost;
+}
+
 InstructionCost TargetTransformInfo::getInsertExtractValueCost(
     unsigned Opcode, TTI::TargetCostKind CostKind) const {
   assert((Opcode == Instruction::InsertValue ||
