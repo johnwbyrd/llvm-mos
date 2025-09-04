3c59f90136070c2770ec1bbeb13ba362fc9d8a79
Hassnaa Hamdi <hassnaa.hamdi@arm.com>
2025-09-04 19:31:30 +0100

    Prefer epilogue with fixed-width over scalable VF. (#155546)
    In case of equal costs Prefer epilogue with fixed-width over scalable VF.
    That is helpful in cases like post-LTO vectorization where epilogue with
    fixed-width VF can be removed when we eventually know that the trip count
    is less than the epilogue iterations.

--- llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
+++ llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
@@ -1108,7 +1108,9 @@ public:
     return VF;
   }
 
-  virtual bool preferFixedOverScalableIfEqualCost() const { return false; }
+  virtual bool preferFixedOverScalableIfEqualCost(bool IsEpilogue) const {
+    return false;
+  }
 
   virtual bool preferInLoopReduction(RecurKind Kind, Type *Ty) const {
     return false;
