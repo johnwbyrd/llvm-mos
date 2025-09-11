494354ca55869aad318f3af1953cd867858f7075
Shunsuke Watanabe <watanabe.shu-06@fujitsu.com>
2025-09-11 10:26:51 +0900

    Improve warning message for complex range overrides (#154899)
    This patch improves the warnings to show which user options override the
    complex range. When a complex range is overridden, explicitly display
    both the option name and the implied complex range value for both the
    overriding and the overridden options.
    
    See also the discussion in the following discourse post:
    https://discourse.llvm.org/t/the-priority-of-fno-fast-math-regarding-complex-number-calculations/84679

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -3566,8 +3566,9 @@ std::string tools::complexRangeKindToStr(LangOptions::ComplexRangeKind Range) {
   case LangOptions::ComplexRangeKind::CX_Promoted:
     return "promoted";
     break;
-  default:
-    return "";
+  case LangOptions::ComplexRangeKind::CX_None:
+    return "none";
+    break;
   }
 }
 
