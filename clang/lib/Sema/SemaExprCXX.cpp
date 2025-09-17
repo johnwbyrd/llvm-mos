adafffa96a567796d30a8b6347848d1c8687d36b
Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>
2025-09-17 10:00:01 +0200

    Align scalar deleting destructors with MSABI (#139566)
    While working on vector deleting destructors support
    ([GH19772](https://github.com/llvm/llvm-project/issues/19772)), I
    noticed that MSVC produces different code in scalar deleting destructor
    body depending on whether class defined its own operator delete. In
    MSABI deleting destructors accept an additional implicit flag parameter
    allowing some sort of flexibility. The mismatch I noticed is that
    whenever a global operator delete is called, i.e. `::delete`, in the
    code produced by MSVC the implicit flag argument has a value that makes
    the 3rd bit set, i.e. "5" for scalar deleting destructors "7" for vector
    deleting destructors.
    Prior to this patch, clang handled `::delete` via calling global
    operator delete direct after the destructor call and not calling class
    operator delete in scalar deleting destructor body by passing "0" as
    implicit flag argument value. This is fine until there is an attempt to
    link binaries compiled with clang with binaries compiled with MSVC. The
    problem is that in binaries produced by MSVC the callsite of the
    destructor won't call global operator delete because it is assumed that
    the destructor will do that and a destructor body generated by clang
    will never do.
    This patch removes call to global operator delete from the callsite and
    add additional check of the 3rd bit of the implicit parameter inside of
    scalar deleting destructor body.
    
    ---------
    
    Co-authored-by: Tom Honermann <tom@honermann.net>

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -3581,7 +3581,7 @@ void Sema::DeclareGlobalAllocationFunction(DeclarationName Name,
 FunctionDecl *
 Sema::FindUsualDeallocationFunction(SourceLocation StartLoc,
                                     ImplicitDeallocationParameters IDP,
-                                    DeclarationName Name) {
+                                    DeclarationName Name, bool Diagnose) {
   DeclareGlobalNewDelete();
 
   LookupResult FoundDelete(*this, Name, StartLoc, LookupOrdinaryName);
@@ -3596,7 +3596,7 @@ Sema::FindUsualDeallocationFunction(SourceLocation StartLoc,
   if (!Result)
     return nullptr;
 
-  if (CheckDeleteOperator(*this, StartLoc, StartLoc, /*Diagnose=*/true,
+  if (CheckDeleteOperator(*this, StartLoc, StartLoc, Diagnose,
                           FoundDelete.getNamingClass(), Result.Found,
                           Result.FD))
     return nullptr;
@@ -3607,7 +3607,8 @@ Sema::FindUsualDeallocationFunction(SourceLocation StartLoc,
 
 FunctionDecl *Sema::FindDeallocationFunctionForDestructor(SourceLocation Loc,
                                                           CXXRecordDecl *RD,
-                                                          bool Diagnose) {
+                                                          bool Diagnose,
+                                                          bool LookForGlobal) {
   DeclarationName Name = Context.DeclarationNames.getCXXOperatorName(OO_Delete);
 
   FunctionDecl *OperatorDelete = nullptr;
@@ -3616,18 +3617,20 @@ FunctionDecl *Sema::FindDeallocationFunctionForDestructor(SourceLocation Loc,
       DeallocType, ShouldUseTypeAwareOperatorNewOrDelete(),
       AlignedAllocationMode::No, SizedDeallocationMode::No};
 
-  if (FindDeallocationFunction(Loc, RD, Name, OperatorDelete, IDP, Diagnose))
-    return nullptr;
+  if (!LookForGlobal) {
+    if (FindDeallocationFunction(Loc, RD, Name, OperatorDelete, IDP, Diagnose))
+      return nullptr;
 
-  if (OperatorDelete)
-    return OperatorDelete;
+    if (OperatorDelete)
+      return OperatorDelete;
+  }
 
   // If there's no class-specific operator delete, look up the global
   // non-array delete.
   IDP.PassAlignment = alignedAllocationModeFromBool(
       hasNewExtendedAlignment(*this, DeallocType));
   IDP.PassSize = SizedDeallocationMode::Yes;
-  return FindUsualDeallocationFunction(Loc, IDP, Name);
+  return FindUsualDeallocationFunction(Loc, IDP, Name, Diagnose);
 }
 
 bool Sema::FindDeallocationFunction(SourceLocation StartLoc, CXXRecordDecl *RD,
