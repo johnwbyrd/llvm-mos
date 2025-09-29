e1214bfa219a40429a3704765ab3a2140afe76bd
Andy Kaylor <akaylor@nvidia.com>
2025-09-29 10:29:41 -0700

    Add initial support for operator delete (#160574)
    This adds basic operator delete handling in CIR. This does not yet
    handle destroying delete or array delete, which will be added later. It
    also does not insert non-null checks when not optimizing for size.

--- clang/include/clang/CIR/MissingFeatures.h
+++ clang/include/clang/CIR/MissingFeatures.h
@@ -208,6 +208,7 @@ struct MissingFeatures {
   static bool dataLayoutTypeAllocSize() { return false; }
   static bool dataLayoutTypeStoreSize() { return false; }
   static bool deferredCXXGlobalInit() { return false; }
+  static bool deleteArray() { return false; }
   static bool devirtualizeMemberFunction() { return false; }
   static bool ehCleanupFlags() { return false; }
   static bool ehCleanupScope() { return false; }
@@ -218,6 +219,7 @@ struct MissingFeatures {
   static bool emitCondLikelihoodViaExpectIntrinsic() { return false; }
   static bool emitLifetimeMarkers() { return false; }
   static bool emitLValueAlignmentAssumption() { return false; }
+  static bool emitNullCheckForDeleteCalls() { return false; }
   static bool emitNullabilityCheck() { return false; }
   static bool emitTypeCheck() { return false; }
   static bool emitTypeMetadataCodeForVCall() { return false; }
