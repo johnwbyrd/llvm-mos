8b5a8a16875b317f5f6a87b65f5fab1c60aa207d
Maryam Moghadas <maryammo@ca.ibm.com>
2025-09-30 09:16:43 -0400

    Add __dmr2048 type and DMF crypto builtins (#157152)
    Define the __dmr2048 type to represent the DMR pair introduced by the
    Dense Math Facility on PowerPC, and add three Clang builtins
    corresponding to DMF cryptography:
    
    __builtin_mma_dmsha2hash
    __builtin_mma_dmsha3hash
    __builtin_mma_dmxxshapad
    
    The __dmr2048 type is required for the dmsha3hash crypto builtin, and,
    as withother PPC MMA and DMR types, its use is strongly restricted.

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -3508,6 +3508,7 @@ static void encodeTypeForFunctionPointerAuth(const ASTContext &Ctx,
     case BuiltinType::VectorQuad:
     case BuiltinType::VectorPair:
     case BuiltinType::DMR1024:
+    case BuiltinType::DMR2048:
       OS << "?";
       return;
 
